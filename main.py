f4dd2c7e4354d95c40d8b3372f98f1b72e39ed98
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-04-16 18:48:26 -0400

    Completed initial conception of bump code

--- main.py
+++ main.py
@@ -10,23 +10,26 @@ GEN_FRAMES = 50
 black = Image.new("RGB", (1920, 1080), (0, 0, 0))
 video = cv2.VideoWriter("video.mp4", cv2.VideoWriter_fourcc(*'mp4v'), FPS, (1920, 1080))
 
-points = [(0, 0), (1, 0)]
+points = [(100, 540), (1820, 540)]
 points_new = []
 
 def progress_point(a: tuple, b: tuple, progress: int):
     return (a[0] * progress + b[0] * (1 - progress), a[1] * progress + b[1] * (1 - progress))
 
 def gen_bump(a, b, length):
-  length *= 1/3
-  
-  points_new.append((
-    (a[1] > b[1]) * length + -(a[1] < b[1]) * length,
-    (a[1] < b[1]) * length + -(a[1] > b[1]) * length
-  ))
-  points_new.append(
-    points_new[-1][0],
-    points_new[-1][1]
-  )
+    global points_new
+    length *= 1/3
+    direction = {}
+
+    p1 = (
+        (a[1] > b[1]) * length - (a[1] < b[1]) * length,
+        (a[0] > b[0]) * length - (a[0] < b[0]) * length
+    )
+    points_new.append(p1)
+    points_new.append(
+        p1[0] + b[0] - a[0],
+        p1[1] + b[1] - a[1]
+    )
 
 for generation in range(GENS):
     for point in range(len(points - 1)):
@@ -55,12 +58,9 @@ for generation in range(GENS):
 
         points_new.append(points[point])
         points_new.append(base_1)
-        points_new.append((
-            {False: base_1}[points[point][0] == base_1],
-            {False: base_1}[points[point][0] == base_1]
-        ))
-        points_new.append(({}, {}))
+        gen_bump(base_1, base_2)
         points_new.append(base_2)
+        
     for frame in range(GEN_FRAMES):
         pass
 
