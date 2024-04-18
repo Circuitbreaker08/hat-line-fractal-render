4251779ce8d28ee9b8a7e373b297c7e68828eb48
Circuitbreaker08 <circuitbreaker08@gmail.com>
2024-04-17 20:51:28 -0400

    Corrected runtime errors

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PIL import Image
 
 import numpy
+import math
 import cv2
 
 FPS = 25
@@ -14,9 +15,9 @@ points = [(100, 540), (1820, 540)]
 def progress_point(a: tuple, b: tuple, progress: int):
     return (a[0] * progress + b[0] * (1 - progress), a[1] * progress + b[1] * (1 - progress))
 
-def gen_bump(a, b, length):
+def gen_bump(a, b):
     global points_final
-    length *= 1/3
+    length = math.dist(a, b)
     direction = {}
 
     p1 = (
@@ -24,15 +25,15 @@ def gen_bump(a, b, length):
         (a[0] > b[0]) * length - (a[0] < b[0]) * length
     )
     points_final.append(p1)
-    points_final.append(
+    points_final.append((
         p1[0] + b[0] - a[0],
         p1[1] + b[1] - a[1]
-    )
+    ))
 
 for generation in range(GENS):
     points_new = []
     points_final = []
-    for point in range(len(points - 1)):
+    for point in range(len(points) - 1):
         points_new.append(points[point]) #point itself
         base_1 = progress_point( #first peak of protrusion
             points[point],
@@ -53,13 +54,16 @@ for generation in range(GENS):
         points_new.append(base_2)
         points_new.append(progress_point( #second base of protrustion
             base_2,
-            points[point + 1]
+            points[point + 1],
+            2/3
         ))
 
         points_final.append(points[point])
         points_final.append(base_1)
         gen_bump(base_1, base_2)
         points_final.append(base_2)
+        print(points_new)
+        print(points_final)
 
     for frame in range(GEN_FRAMES):
         pass
